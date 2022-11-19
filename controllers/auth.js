e3bee9b86cabf7c1cbae22d5b1c06ad7da987298
phunguyen25 <phunv@devcamp.edu.vn>
2022-11-19 17:46:47 +0700

    udpate

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,7 +40,7 @@ const sendMail = async (emailUser, token) => {
       subject: "Password Reset",
       html: `
       <p>You requested a reset password</p>
-      <p>Click this <a href='https://pishopadmin.netlify.app/reset/${token}'>link</a> to set new password</p>
+      <p>Click this <a href='https://pishop.netlify.app/reset/${token}'>link</a> to set new password</p>
     `,
     });
     console.log("success!");
